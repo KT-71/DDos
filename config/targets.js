c365ba7fa710626de6c2670ea2ac8b65d8514f87
K.T <z1022001@gmail.com>
2022-11-18 09:08:55 +0800

    v0.1.7
    auto build login.js
    add new target

--- config/targets.js
+++ config/targets.js
@@ -11,6 +11,7 @@ module.exports = {
         // `https://twitter.com/tishnf2`,
         `https://twitter.com/denote17494172`,
         `https://twitter.com/REI92648622`,
+        `https://twitter.com/kis_krrrri`,
         // `https://twitter.com/denote17494172/status/1592399730868649985`,
         // `https://twitter.com/denote17494172/status/1592399234472742913`,
         `https://twitter.com/kls_kirrrrrr/status/1589102320914419712`,
@@ -18,6 +19,7 @@ module.exports = {
         `https://twitter.com/kis_klrrrrr/status/1591540371557081088`,
         `https://twitter.com/kis_klrrrrr/status/1591269345438892032`,
         `https://twitter.com/REI92648622/status/1592517065034067968`,
+        `https://twitter.com/kis_krrrri/status/1593131886582595584`,
+        `https://twitter.com/kis_krrrri/status/1593203894427189248`,
     ]
-
 }
--- index.js
+++ index.js
@@ -476,6 +476,16 @@ const main = async () => {
         await driver.quit();
     }
 
+    // check login config
+    if (!fs.existsSync(`./config/login.js`)) {
+        fs.writeFileSync(`./config/login.js`, [
+            `module.exports = {`, `    logins: [`, `        {`,
+            `            email: '',	        // 登入用信箱`,
+            `            password: '',		// 使用者密碼, 若要手動登入此處留空即可`,
+            `            username: ''	    // 本腳本較容易被 twitter 判定為可疑登入, 故需要覆核使用者名稱`,
+            `        },`, `    ]`, `}`
+        ].join('\r\n'));
+    }
 
     // main var
     const { logins } = require(`./config/login.js`);
