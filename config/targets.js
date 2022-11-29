b42c6e667cf2829ede879d65a2053e5a751b7ffb
K.T <z1022001@gmail.com>
2022-11-29 09:17:43 +0800

    new target list

--- config/targets.js
+++ config/targets.js
@@ -1,28 +1,9 @@
 module.exports = {
-    
+
     realUsername: `kis_kirrrrrr`,   // real user's username
 
     // fake user
     targets: [
-        // `https://twitter.com/kls_kirrrrrr`,
-        // `https://twitter.com/kls_kirrrrrr/status/1589102320914419712`,
-
-        // `https://twitter.com/least57`,
-        
-        // `https://twitter.com/kis_klrrrrr`,
-        // `https://twitter.com/kis_klrrrrr/status/1591770511281115138`,
-        // `https://twitter.com/kis_klrrrrr/status/1591540371557081088`,
-        // `https://twitter.com/kis_klrrrrr/status/1591269345438892032`,
-
-        // `https://twitter.com/tishnf2`,
-
-        // `https://twitter.com/denote17494172`,
-        // `https://twitter.com/denote17494172/status/1592399730868649985`,
-        // `https://twitter.com/denote17494172/status/1592399234472742913`,
-
-        // `https://twitter.com/ke445nl2`,
-        // `https://twitter.com/ke445nl2/status/1593028905333854210`,
-        
         `https://twitter.com/kis_krrrri`,
         `https://twitter.com/kis_krrrri/status/1593131886582595584`,
         `https://twitter.com/kis_krrrri/status/1593203894427189248`,
@@ -32,18 +13,33 @@ module.exports = {
         `https://twitter.com/kis_krrrri/status/1593574418244329473`,
         `https://twitter.com/kis_krrrri/status/1593887414241660928`,
 
-        // `https://twitter.com/REI92648622`,
-        // `https://twitter.com/REI92648622/status/1592517065034067968`,
-
-        // `https://twitter.com/XIfKIKRmFCEY5Wz`,
-        // `https://twitter.com/XIfKIKRmFCEY5Wz/status/1595114784638513154`,
-        // `https://twitter.com/XIfKIKRmFCEY5Wz/status/1595113583079469058`,
+        `https://twitter.com/Crq71626722`,
+        `https://twitter.com/Crq71626722/status/1597169240708362241`,
+        `https://twitter.com/Crq71626722/status/1597160936426520576`,
+        `https://twitter.com/Crq71626722/status/1597149296662831105`,
+        `https://twitter.com/Crq71626722/status/1597147909102784512`,
+        `https://twitter.com/Crq71626722/status/1597099793171755008`,
+        `https://twitter.com/Crq71626722/status/1597098621232590849`,
+        `https://twitter.com/Crq71626722/status/1597083411142746112`,
+        `https://twitter.com/Crq71626722/status/1597349615858176002`,
 
         `https://twitter.com/Japan18420827`,
-        `https://twitter.com/Japan18420827/status/1596870926343495688`,
-        `https://twitter.com/Japan18420827/status/1596869813703708674`,
-        `https://twitter.com/Japan18420827/status/1596899966383161345`,
+        `https://twitter.com/Japan18420827/status/1597163587222777857`,
+        `https://twitter.com/Japan18420827/status/1597162664773701632`,
+        `https://twitter.com/Japan18420827/status/1597122526710476800`,
+        `https://twitter.com/Japan18420827/status/1597071678236168194`,
         `https://twitter.com/Japan18420827/status/1596941728808075264`,
+        `https://twitter.com/Japan18420827/status/1596901839643246592`,
+        `https://twitter.com/Japan18420827/status/1596899966383161345`,
+
+        `https://twitter.com/b4AyFyB3fl0LjEH`,
+        `https://twitter.com/b4AyFyB3fl0LjEH/status/1595338125890707456`,
+        `https://twitter.com/b4AyFyB3fl0LjEH/status/1595337753453117445`,
+
+        `https://twitter.com/kkir25438962`,
+        `https://twitter.com/kkir25438962/status/1595757820661886976`,
+
+
     ]
 }
 
