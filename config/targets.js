fd2404a42952bfdd82de707d55fa1352da6b78a1
K.T <z1022001@gmail.com>
2022-12-01 11:56:31 +0800

    new target list

--- config/targets.js
+++ config/targets.js
@@ -4,25 +4,6 @@ module.exports = {
 
     // fake user
     targets: [
-        `https://twitter.com/kis_krrrri`,
-        `https://twitter.com/kis_krrrri/status/1593131886582595584`,
-        `https://twitter.com/kis_krrrri/status/1593203894427189248`,
-        `https://twitter.com/kis_krrrri/status/1593329867843682306`,
-        `https://twitter.com/kis_krrrri/status/1593394401392431111`,
-        `https://twitter.com/kis_krrrri/status/1593523672903389185`,
-        `https://twitter.com/kis_krrrri/status/1593574418244329473`,
-        `https://twitter.com/kis_krrrri/status/1593887414241660928`,
-
-        `https://twitter.com/Crq71626722`,
-        `https://twitter.com/Crq71626722/status/1597169240708362241`,
-        `https://twitter.com/Crq71626722/status/1597160936426520576`,
-        `https://twitter.com/Crq71626722/status/1597149296662831105`,
-        `https://twitter.com/Crq71626722/status/1597147909102784512`,
-        `https://twitter.com/Crq71626722/status/1597099793171755008`,
-        `https://twitter.com/Crq71626722/status/1597098621232590849`,
-        `https://twitter.com/Crq71626722/status/1597083411142746112`,
-        `https://twitter.com/Crq71626722/status/1597349615858176002`,
-
         `https://twitter.com/Japan18420827`,
         `https://twitter.com/Japan18420827/status/1597163587222777857`,
         `https://twitter.com/Japan18420827/status/1597162664773701632`,
@@ -32,13 +13,13 @@ module.exports = {
         `https://twitter.com/Japan18420827/status/1596901839643246592`,
         `https://twitter.com/Japan18420827/status/1596899966383161345`,
         `https://twitter.com/Japan18420827/status/1597578526231781376`,
-
-        `https://twitter.com/b4AyFyB3fl0LjEH`,
-        `https://twitter.com/b4AyFyB3fl0LjEH/status/1595338125890707456`,
-        `https://twitter.com/b4AyFyB3fl0LjEH/status/1595337753453117445`,
+        `https://twitter.com/Japan18420827/status/1597820148278521858`,
 
         `https://twitter.com/kkir25438962`,
         `https://twitter.com/kkir25438962/status/1595757820661886976`,
+
+        `https://twitter.com/Crqzybe81562546`,
+        `https://twitter.com/Crqzybe81562546/status/1597576810707177473`,
 
 
     ]
