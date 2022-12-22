c8fa4c90acd0774e59ed49a6501a5497268c3e8d
K.T <z1022001@gmail.com>
2022-12-22 11:06:03 +0800

    new target list

--- config/targets.js
+++ config/targets.js
@@ -4,72 +4,67 @@ module.exports = {
 
     // fake user
     targets: [
+        // 22/11/28
         `https://twitter.com/Japan18420827`,
         `https://twitter.com/Japan18420827/status/1599706259804258304`,
         `https://twitter.com/Japan18420827/status/1599702904688226304`,
         `https://twitter.com/Japan18420827/status/1599349687576035329`,
 
+        // 22/12/07
         `https://twitter.com/87T19`,
 
+        // 22/12/08
         `https://twitter.com/Revok51939878`,
         `https://twitter.com/Revok51939878/status/1600704959380541446`,
         `https://twitter.com/Revok51939878/status/1600647273603600384`,
         `https://twitter.com/Revok51939878/status/1600575069658431560`,
 
+        // 22/12/09
         `https://twitter.com/Kishxxxir`,
-        
+
+        // 22/12/18
         `https://twitter.com/Rydirxxirr`,
         `https://twitter.com/Rydirxxirr/status/1603806481815519232`,
         `https://twitter.com/Rydirxxirr/status/1603795233686396928`,
-        
+
         `https://twitter.com/Ladies99420028`,
         `https://twitter.com/Ladies99420028/status/1603771698452795392`,
-        
+
         `https://twitter.com/AlfvreRa_2`,
         `https://twitter.com/AlfvreRa_2/status/1603716448227954688`,
         `https://twitter.com/AlfvreRa_2/status/1603714571583328257`,
         `https://twitter.com/AlfvreRa_2/status/1603365209514840070`,
-        
+
         `https://twitter.com/OcVqEgtgvmgbvHK`,
         `https://twitter.com/OcVqEgtgvmgbvHK/status/1602753413284040708`,
-        
-        `https://twitter.com/Regions26895642`,
-        `https://twitter.com/Regions26895642/status/1604998821330661377`,
-        `https://twitter.com/Regions26895642/status/1604993349676851200`,
-        `https://twitter.com/Regions26895642/status/1604812145618731010`,
-        `https://twitter.com/Regions26895642/status/1604808843846094849`,
-        `https://twitter.com/Regions26895642/status/1604607342448087040`,
-        
+
+        // 22/12/19
         `https://twitter.com/EagleEy67790683`,
+        `https://twitter.com/EagleEy67790683/status/1605616732378275840`,
         `https://twitter.com/EagleEy67790683/status/1605349890804125697`,
         `https://twitter.com/EagleEy67790683/status/1605346155369570304`,
         `https://twitter.com/EagleEy67790683/status/1605185771165683715`,
         `https://twitter.com/EagleEy67790683/status/1604816783625650178`,
         `https://twitter.com/EagleEy67790683/status/1604810841773277185`,
-        
-        `https://twitter.com/revfak`,
-        `https://twitter.com/revfak/status/1605305767124275200`,
-        `https://twitter.com/revfak/status/1604837722891423744`,
-        `https://twitter.com/revfak/status/1604825732416507906`,
-        `https://twitter.com/revfak/status/1604818660937576448`,
 
-        `https://twitter.com/Reve53`,
-        `https://twitter.com/Reve53/status/1605305317025353728`,
-        `https://twitter.com/Reve53/status/1605196443706609664`,
-        
-        `https://twitter.com/Tara37Ravens`,
-        `https://twitter.com/Reve53/status/1605183589934665728`,
-        
+        // 22/12/22
+        `https://twitter.com/Raven94336045`,
+        `https://twitter.com/Raven94336045/status/1605615407821225985`,
+        `https://twitter.com/Raven94336045/status/1605565883593564161`,
+        `https://twitter.com/Raven94336045/status/1605565116279242752`,
+
+        `https://twitter.com/Denen38515700`,
+        `https://twitter.com/Denen38515700/status/1605572113548554245`,
+        `https://twitter.com/Denen38515700/status/1605570175679488006`,
+
+
         `https://twitter.com/crdians`,
         `https://twitter.com/Iliac21944663`,
         `https://twitter.com/Alpmtmsmi3826`,
         `https://twitter.com/jit08749510`,
-        `https://twitter.com/CsT37283310`,
-        `https://twitter.com/DrF1k1`,
         `https://twitter.com/alterations696`,
     ]
 }
-
 
 
 
