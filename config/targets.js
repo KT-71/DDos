639e856901e7efd97eb32e9259a4400fbedd7cb3
K.T <z1022001@gmail.com>
2023-01-11 13:47:09 +0800

    new target list

--- config/targets.js
+++ config/targets.js
@@ -12,7 +12,6 @@ module.exports = {
 
         // 22/12/08
         `https://twitter.com/Revok51939878`,
-        `https://twitter.com/Revok51939878/status/1600704959380541446`,
         `https://twitter.com/Revok51939878/status/1600647273603600384`,
         `https://twitter.com/Revok51939878/status/1600575069658431560`,
 
@@ -33,10 +32,6 @@ module.exports = {
         // 22/12/27
         `https://twitter.com/Did_i_xrpxxirr`,
         `https://twitter.com/Did_i_xrpxxirr/status/1607514517927497729`,
-
-        `https://twitter.com/Industr00428009`,
-        `https://twitter.com/Industr00428009/status/1607709212410523653`,
-        `https://twitter.com/Industr00428009/status/1607529280891658241`,
 
         // 22/12/30
         `https://twitter.com/diesard`,
@@ -61,32 +56,15 @@ module.exports = {
         `https://twitter.com/75joafowj/status/1610859648017469440`,
 
         // 23/01/08
-
         `https://twitter.com/expressairk`,
 
         // 23/01/08
-        `https://twitter.com/Rightart3`,
-
         `https://twitter.com/5Pnq09G9eOfbZRf`,
         `https://twitter.com/5Pnq09G9eOfbZRf/status/1612588708183834624`,
 
         `https://twitter.com/WcJOus1K3QYkDbR`,
 
         // 23/01/10
-        `https://twitter.com/Nayuta10108`,
-        `https://twitter.com/Nayuta10108/status/1612591866901594113`,
-        `https://twitter.com/Nayuta10108/status/1612590980167000064`,
-        `https://twitter.com/Nayuta10108/status/1604131709301276675`,
-        `https://twitter.com/Nayuta10108/status/1604129416187084802`,
-        `https://twitter.com/Nayuta10108/status/1603808091543420931`,
-        `https://twitter.com/Nayuta10108/status/1603230223276789760`,
-        `https://twitter.com/Nayuta10108/status/1603016244554215424`,
-        `https://twitter.com/Nayuta10108/status/1602992852455329792`,
-        `https://twitter.com/Nayuta10108/status/1602992613706981378`,
-        `https://twitter.com/Nayuta10108/status/1602699872989315076`,
-        
-        `https://twitter.com/Deejay64626526`,
-
         `https://twitter.com/Laur31068381`,
 
 
